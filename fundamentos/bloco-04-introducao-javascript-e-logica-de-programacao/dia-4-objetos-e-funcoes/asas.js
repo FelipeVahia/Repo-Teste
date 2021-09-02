be210990ca81873c0c7f19ca9f9967a899fa333c
FelipeVahia <felipevahia@gmail.com>
2021-09-02 19:50:48 -0300

    Bloco5-exercicio1-adicionado

--- fundamentos/bloco-04-introducao-javascript-e-logica-de-programacao/dia-4-objetos-e-funcoes/asas.js
+++
@@ -1,9 +0,0 @@
-function verificaPalindrome(word){
-  for(index in word){
-    if(word[index] != word[(word.length - 1) - index]){
-      return false;
-    }
-  }
-  return true;
-}
-console.log(verificaPalindrome('desenvolvimento'));
