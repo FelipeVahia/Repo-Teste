24e588af023d4d99ff5a1e066dd3d666ed08f337
FelipeVahia <felipevahia@gmail.com>
2021-10-27 21:18:46 -0300

    Implement  front-end-block-and-first-exercises

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/fixacao/src/Component.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class WhoAmI extends Component {
+  render() {
+    return <p>Sou o Felipe Vahia Malliagros, tenho 21 anos e moro no Rio de Janeiro.</p>
+  }
+}
+
+export default WhoAmI;
