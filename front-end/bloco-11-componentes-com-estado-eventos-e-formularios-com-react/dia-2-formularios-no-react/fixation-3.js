34a14d276e6de8d51ee30d2c2e1608d0a8dca015
FelipeVahia <felipevahia@gmail.com>
2021-11-05 04:14:06 -0300

    Implement exercises of block11-day2

---
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/fixation-3.js
@@ -0,0 +1,86 @@
+// All components controlled
+
+import React, { Component } from 'react';
+
+class Form extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: '',
+      email: '',
+      age: '',
+      anecdote: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  render() {
+    const { name, email, age, anecdote } = this.state;
+
+    return (
+      <div>
+        <h1>Estados e React - Tecnologia fantástica ou reagindo a regionalismos?</h1>
+        <form className="form">
+
+          <label htmlFor="name">
+            Nome:
+            <input
+              id="name"
+              name="name"
+              type="text"
+              onChange={ this.handleChange }
+              value={ name }
+            />
+          </label>
+
+          <label htmlFor="email">
+            Email:
+            <input
+              id="email"
+              name="email"
+              type="email"
+              onChange={ this.handleChange }
+              value={ email }
+            />
+          </label>
+
+          <label htmlFor="age">
+            Idade:
+            <select
+              id="age"
+              name="age"
+              onChange={ this.handleChange }
+              value={ age }
+            >
+              <option value="">Selecione</option>
+              <option value="adult">Maior que 18</option>
+              <option value="underage">Menor que 18</option>
+            </select>
+          </label>
+
+          <label htmlFor="anecdote">
+            Anedota:
+            <textarea
+              id="anecdote"
+              name="anecdote"
+              onChange={ this.handleChange }
+              value={ anecdote }
+            />
+          </label>
+
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Form;
